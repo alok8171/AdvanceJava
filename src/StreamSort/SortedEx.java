87b4a0532ad09fc5de9304759117c7bdcea5cf11
Alok <alok.25161020@kiet.edu>
2026-03-24 10:34:14 +0530

    StreamAPImap

---
+++ src/StreamSort/SortedEx.java
@@ -0,0 +1,64 @@
+package StreamSort;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class SortedEx {
+    public static void main(String[] args) {
+//        List <Integer> list=Arrays.asList(23,12,1,45,23,76,26,91);
+//        System.out.println(list);
+//        List <Integer> list2= list.stream().sorted((a,b)->b-a).toList();// it is valid
+//        List<Integer> sortedList=list.stream().sorted(Comparator.reverseOrder()).toList();// it is also valid
+//        System.out.println(sortedList);
+//        System.out.println(list2);
+
+
+        //1.sort list in ascending order
+//        List<Integer> list=Arrays.asList(23,14,1,85,23,76,23,91);
+//        System.out.println(list);
+//        List<Integer> sortedList=list.stream().sorted((a,b)->a-b).toList();
+//        System.out.println(sortedList);
+
+        //3.sort the list and print top 3 highest number
+//        List<Integer> list=Arrays.asList(23,14,1,85,23,76,23,91);
+//        System.out.println(list);
+//        List<Integer> sortedlist=list.stream().sorted((a,b)->b-a).limit(3).toList();
+//        System.out.println(sortedlist);
+
+        //4.sort even number
+//        List<Integer> list = Arrays.asList(23, 14, 1, 85, 23, 76, 23, 91);
+//        List<Integer> sortedlist = list.stream().sorted().filter(n -> n % 2 == 0).toList();
+//        System.out.println(sortedlist);
+
+
+        //7.second highest
+//                List<Integer> list=Arrays.asList(23,14,1,85,23,76,23,91);
+//               System.out.println(list);
+//               Integer second=list.stream().sorted(Comparator.reverseOrder()).toList().get(1);
+//               System.out.println(second);
+
+        //8.sort gretaer than 20
+//       List<Integer> list=Arrays.asList(33,14,1,85,23,76,43,91);
+//       System.out.println(list);
+//       List<Integer> sortedlist = list.stream().sorted().filter(n -> n >20 ).toList();
+//       System.out.println(sortedlist);
+
+//    List<String> list = Arrays.asList("ravi", "ankit", "zoya", "meena", "kunal");
+//  System.out.println(list);
+
+  // 11.
+//        List<String> sortedlist = list.stream().sorted().toList();
+//        System.out.println(sortedlist);
+
+        //12.
+//  List<String> sortedList=list.stream().sorted((a,b)->b.compareTo(a)).toList();
+//  System.out.println(sortedList);
+
+        // 13.
+//        List<String> sortedlist=list.stream().sorted((a,b)->b.length()-a.length()).toList();
+//        System.out.println(sortedlist);
+
+
+
+    }
+}
+
